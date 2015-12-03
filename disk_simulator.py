90c3b720edd10f4164a8809813d35f2c004a8495
Pulak Goyal <pulakgoyal1@gmail.com>
2015-12-03 18:24:07 -0500

    Added worst case interval tree script

--- disk_simulator.py
+++ disk_simulator.py
@@ -4,6 +4,8 @@ import re
 import os 
 import mmap
 import directio
+import time
+import numpy 
 
 global t
 t = IntervalTree()
@@ -23,6 +25,11 @@ def main():
 
 	# Open disk for reading and writing 
 	disk = directio.DirectFile(str(sys.argv[2]), 512)
+
+	# Keep track of total time spend using the interval tree
+	counter = 0;
+	time_list_read = []
+	time_list_write = []
 
 	while(True):
 
@@ -44,10 +51,14 @@ def main():
 			new_interval = Interval(int(match.group(3)), int(match.group(3)) + int(match.group(4)))
 
 			# Add new interval to tree
+			start = time.time()
 			if (t.overlaps(new_interval[0], new_interval[1])):
 				partition_overlapping_intervals(new_interval)
 			else:
 				t.add(new_interval)
+			end = time.time()
+			time_list_read.append(end - start)
+			counter += end - start
 
 		# Match on disk read
 		match = re.search(r"([D]\s*) ([R]) (\d+) \+ (\d+)", line)
@@ -55,13 +66,33 @@ def main():
 		if(match != None):
 
 			# Read from disk
-			for interval in t.search(int(match.group(3)), int(match.group(3)) + int(match.group(4))):
+			start = time.time()
+			interval_list = t.search(int(match.group(3)), int(match.group(3)) + int(match.group(4)))
+			end = time.time()
+			time_list_write.append(end - start)
+			counter += end - start
+			for interval in interval_list:
 				start = interval[0]
 				length = interval[1] - interval[0]
 				disk.pread(start*512, length * 512)
 
-	inptr.close()
+	inptr.close() 
 	disk.release()
+
+	print str(sys.argv[1]) + ": " + str(counter)
+
+	f1 = open("read_distribution.txt", "a")
+	f1.write("time_list_read for " + str(sys.argv[1]) + "\n")
+	hist1 = numpy.histogram(time_list_read, bins=50)
+	f1.write(str(hist1))
+	f1.close
+
+
+	f2 = open("write_distribution.txt", "a")
+	f2.write("time_list_write for " + str(sys.argv[1]) + "\n")
+	hist2 = numpy.histogram(time_list_write, bins=50)
+	f2.write(str(hist2))
+	f2.close
 
 if __name__ == '__main__':
    main()
